5b02be3c3c753a8f8649e1bc234a758b69f76c43
rumite <bakhtiyarowtn@gmail.com>
2021-10-20 18:20:33 +0300

    Add ChromeDriver selection and constants architecture

---
+++ src/main/java/common/Config.java
@@ -0,0 +1,12 @@
+package common;
+
+public class Config {
+
+    /**
+     * Specify the browser and platform for test:
+     *
+     * CHROME_WINDOWS
+     * CHROME_MAC
+     */
+    public static final String BROWSER_AND_PLATFORM = "";
+}
