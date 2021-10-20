75df94e75868ef2308b1317b5bcad6ae1743ef7b
rumite <bakhtiyarowtn@gmail.com>
2021-10-20 18:44:17 +0300

    Create BaseTest and BasePage

--- src/main/java/common/Config.java
+++ src/main/java/common/Config.java
@@ -8,5 +8,7 @@ public class Config {
      * CHROME_WINDOWS
      * CHROME_MAC
      */
-    public static final String BROWSER_AND_PLATFORM = "";
+    public static final String BROWSER_AND_PLATFORM = "CHROME_WINDOWS";
+    public static final Boolean CLEAR_COOKIES = true;
+    public static final Boolean HOLD_BROWSER_OPEN = true;
 }
